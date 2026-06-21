1ab5f2bf66cbc979ad8d93bb1f4a42c67932a657
kaua_ananiass <kauaananias11@gmail.com>
2026-06-20 22:58:53 -0300

    atualizacao 1.2

--- static/supabase-config.js
+++
@@ -1,5 +0,0 @@
-export const SUPABASE_URL = "https://aokulbaetrjarubmlyld.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_G9KnTqJB2g9VWTuFeKKHUQ_O6Zn_i4A";
-
-export const SUPABASE_LIBRARY_TABLE = "led_library";
-export const SUPABASE_PROJECT_AUDIO_BUCKET = "project-audio";
